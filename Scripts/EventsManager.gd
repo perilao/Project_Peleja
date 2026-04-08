a322249727a7a9aa758622795807424bcc2da752
perilao <pdrigot@gmail.com>
2026-04-08 19:10:27 -0300

    0.0.1
    -Grab and Drop item mechanics
    -Different Events added

--- Scripts/EventsManager.gd
+++ Scripts/EventsManager.gd
@@ -1,28 +1,34 @@
 extends StaticBody3D
 
+@export var event_time := 4.0
 @onready var timer_geral: Timer = $timer_geral
+@export var event_name : String = ""
+@onready var player: CharacterBody3D = $"../../Player"
+
 var hasPrinted = false
 var isOnArea = false
+
 
 func _ready() -> void:
 	hasPrinted = false
 	isOnArea = false
 
 func _process(_delta):
-	#print(timer_geral.time_left) #CONTADOR
-	if timer_geral.time_left <= 4.0 and hasPrinted == false: #TROCA DE ESTADO DEPOIS DE CERTO TEMPO
-		print("Faltam 4 segundos!")
+#CONTADOR
+	if timer_geral.time_left <= event_time and hasPrinted == false: #TROCA DE ESTADO DEPOIS DE CERTO TEMPO
+		print("Faltam ", event_time, " segundos!", event_name)
 		hasPrinted = true
-	if Input.is_action_just_pressed("ui_grab") and timer_geral.time_left <= 4.0 and isOnArea == true: #AÇÃO DO ITEM
+	if Input.is_action_just_pressed("ui_interact") and timer_geral.time_left <= event_time and isOnArea == true and player.player_has_item == true: #AÇÃO DO ITEM
 		timer_geral.start()
+		player.release_object()
 		if timer_geral.time_left == timer_geral.wait_time:
 			print("Timer Resetado")
-			hasPrinted = false
+			hasPrinted = false 
 func _on_timer_geral_timeout() -> void: #DERROTA DO PLAYER
 		get_tree().quit() 
 func _on_area_3d_area_entered(area: Area3D) -> void:
-	print("Area ENTERED!")
+	#print("Area ENTERED!")
 	isOnArea = true
 func _on_area_3d_area_exited(area: Area3D) -> void:
-	print("Area EXITED!")
+	#print("Area EXITED!")
 	isOnArea = false
--- Scripts/PlayerMovement.gd
+++ Scripts/PlayerMovement.gd
@@ -1,17 +1,65 @@
 extends CharacterBody3D
-
+#VARIAVEIS
 @export var SPEED = 5.0
+@export var ROTATION_SPEED = 10.0
+@onready var player_raycast: RayCast3D = $Look
+@onready var hand: Node3D = $Hand
+var hold_object: Node3D = null
+var original_layer = 1
+var player_has_item = false
+var original_position: Vector3
+var original_rotation: Vector3
+@onready var player_area: Area3D = $Area3DPlayer
+
+
 
 func _physics_process(delta: float) -> void:
+#GRAVIDADE
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+#INPUT E DIRECAO
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
+	var direction := Vector3(input_dir.x, 0, input_dir.y).normalized()
+#MOVIMENTACAO
+	if direction.length() > 0:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+#ROTACAO
+		var target_angle = atan2(direction.x, direction.z)
+		rotation.y = lerp_angle(rotation.y, target_angle, ROTATION_SPEED * delta)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+#SEGURAR/SOLTAR OBJECTO
+	if hold_object:
+		hold_object.global_position = hand.global_position
+		hold_object.global_rotation = hand.global_rotation
+		if Input.is_action_just_pressed("ui_grab"):
+			release_object()
+	else:
+		if player_raycast.is_colliding():
+			var object = player_raycast.get_collider()
+			if object.is_in_group("pickable"):
+				if Input.is_action_just_pressed("ui_grab"):
+					grab_object(object)
 	move_and_slide()
+
+func grab_object(obj):
+	hold_object = obj
+	original_position = hold_object.global_position
+	original_rotation = hold_object.global_rotation
+	original_layer = obj.collision_layer
+	obj.collision_layer = 0
+	if hold_object is RigidBody3D:
+		hold_object.freeze = true
+		player_has_item = true
+
+func release_object():
+	if hold_object:
+		hold_object.global_position = original_position 
+		hold_object.global_rotation = original_rotation
+		hold_object.collision_layer = original_layer
+		if hold_object is RigidBody3D:
+			hold_object.freeze = false
+			player_has_item = false
+		hold_object = null
