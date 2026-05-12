d9abc5c3c2710b0d3996faefec78ed20563c4184
perilao <pdrigot@gmail.com>
2026-05-12 20:43:54 -0300

    0.0.18
    -Imported changes

--- Scripts/options_menu.gd
+++ Scripts/options_menu.gd
@@ -4,28 +4,27 @@ extends Node
 @onready var sound_manager: Control = $SoundManager
 @onready var controls_manager: Control = $ControlsManager
 @onready var credits_manager: Control = $CreditsManager
+@onready var back: TextureButton = $back
 		
 
 func _on_sound_pressed() -> void:
 	options_manager.visible =  false
 	sound_manager.visible = true
+	back.visible = true
 
 func _on_controls_pressed() -> void:
 	options_manager.visible =  false
 	controls_manager.visible = true
+	back.visible = true
 
 func _on_credits_pressed() -> void:
 	options_manager.visible = false
 	credits_manager.visible = true
-
-func _on_back_3_pressed() -> void:
-	options_manager.visible =  true
-	credits_manager.visible = false
+	back.visible = true
 	
-func _on_back_2_pressed() -> void:
-	options_manager.visible = true
-	controls_manager.visible = false
-
 func _on_back_pressed() -> void:
 	options_manager.visible =  true
 	sound_manager.visible = false
+	controls_manager.visible = false
+	credits_manager.visible = false
+	back.visible = false
