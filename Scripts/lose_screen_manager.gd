e0b49ff7bd17f7b87759e2c5d7c6f08b9d90c7a8
human-et56 <giovanna.andradepmjg@gmail.com>
2026-05-13 17:02:23 -0300

    Lose Screen 0.1

--- Scripts/lose_screen_manager.gd
+++ Scripts/lose_screen_manager.gd
@@ -1,4 +1,7 @@
 extends Control
 
 func _on_restart_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/G.U.I/lvl_selector.tscn")
+
+func _on_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/G.U.I/main_menu.tscn")
